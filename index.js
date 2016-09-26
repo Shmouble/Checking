eb3100d028e903df6deabe3001f440eec191392d
Yura <Shmouble94@mail.ru>
2016-09-26 13:06:14 +0300

    Add data check

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function validateNonEmpty(inputField, helpText) {
-    // See if the input value contains any text
     if (inputField.value.length == 0) {
-        // The data is invalid, so notify the user
         if(helpText != null){
            helpText.innerHTML = "Enter a value, please";
         }
@@ -15,3 +13,79 @@ function validateNonEmpty(inputField, helpText) {
 
     return true;
 }
+
+function validateLength(min, max, inputField, helpText){
+    if(inputField.value.length<min || inputField.value.length>max) {
+        if (helpText != null) {
+            helpText.innerHTML = "Enter 1 to 32 characters";
+        }
+        return false;
+    } else {
+        if (helpText != null){
+            helpText.innerHTML = "";
+        }
+        return true;
+    }
+}
+
+
+//Only for USA ZIP code
+function validateZIPCode(inputField, helpField){
+    if(inputField.value.length != 5){
+        if (helpField != null) {
+            helpField.innerHTML = "You should enter exactly five numerals";
+        }
+        return false;
+    } else if (isNaN(inputField.value)){
+        if (helpField != null) {
+            helpField.innerHTML = "You should enter a number";
+        }
+        return false;
+    } else {
+        if (helpField != null) {
+            helpField.innerHTML = "";
+        }
+        return true;
+    }
+}
+
+
+function placeOrder(form){
+    if(validateLength(1, 32, form["message"], form["message_help"]) &&
+      validateZIPCode(form["zipcode"], form["zipcode_help"]) &&
+      validateNonEmpty(form["date"], form["date_help"]) &&
+      validateNonEmpty(form["name"], form["name_help"]) &&
+      validateNonEmpty(form["phone"], form["phone_help"]) &&
+      validateNonEmpty(form["email"], form["email_help"])){
+        form.submit();
+    } else {
+        alert("Sorry, form filled up with mistakes");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
